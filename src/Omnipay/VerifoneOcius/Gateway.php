1dc45bdd03f88a99370b4576dcf6a25c9e44589c
Glenn Jenkins <glenn@digitickets.co.uk>
2015-09-17 16:43:43 +0100

    Removing some unused code.

--- src/Omnipay/VerifoneOcius/Gateway.php
+++ src/Omnipay/VerifoneOcius/Gateway.php
@@ -23,14 +23,6 @@ class Gateway extends AbstractGateway
             'systemGuid' => ''
         );
     }
-//    public function getPassword()
-//    {
-//        return $this->getParameter('password');
-//    }
-//    public function setPassword($value)
-//    {
-//        return $this->setParameter('password', $value);
-//    }
     
     public function purchase(array $parameters = array())
     {
