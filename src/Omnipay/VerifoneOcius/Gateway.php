0edd103631e6db49d19fd87c34da20b12f30fd0b
Andy Coates <andycoatz@gmail.com>
2015-09-21 17:56:13 +0100

    Autoformat

--- src/Omnipay/VerifoneOcius/Gateway.php
+++ src/Omnipay/VerifoneOcius/Gateway.php
@@ -21,27 +21,32 @@ class Gateway extends AbstractGateway
     {
         return $this->setParameter('accountId', $value);
     }
+
     protected function getAccountId()
     {
         return $this->getParameter('accountId');
     }
+
     public function getMerchantId()
     {
         return $this->getParameter('merchantId');
     }
+
     public function setMerchantId($value)
     {
         return $this->setParameter('merchantId', $value);
     }
+
     public function getSystemGuid()
     {
         return $this->getParameter('systemGuid');
     }
+
     public function setSystemGuid($value)
     {
         return $this->setParameter('systemGuid', $value);
     }
-    
+
     public function getDefaultParameters()
     {
         return array(
@@ -60,77 +65,103 @@ class Gateway extends AbstractGateway
     {
         return $this->getParameter('apiVersion');
     }
+
     public function setApiVersion($value)
     {
         return $this->setParameter('apiVersion', $value);
     }
+
     public function getAllowedPaymentMethods()
     {
         return $this->getParameter('allowedPaymentMethods');
     }
+
     public function setAllowedPaymentMethods($value)
     {
         return $this->setParameter('allowedPaymentMethods', $value);
     }
+
     public function getShowPaymentResult()
     {
         return $this->getParameter('showPaymentResult');
     }
+
     public function setShowPaymentResult($value)
     {
         return $this->setParameter('showPaymentResult', $value);
     }
+
     public function getCaptureMethod()
     {
         return $this->getParameter('captureMethod');
     }
+
     public function setCaptureMethod($value)
     {
         return $this->setParameter('captureMethod', $value);
     }
+
     public function getDeliveryEdit()
     {
         return $this->getParameter('deliveryEdit');
     }
+
     public function setDeliveryEdit($value)
     {
         return $this->setParameter('deliveryEdit', $value);
     }
+
     public function getProcessingIdentifier()
     {
         return $this->getParameter('processingIdentifier');
     }
+
     public function setProcessingIdentifier($value)
     {
         return $this->setParameter('processingIdentifier', $value);
     }
+
     public function getRegisterToken()
     {
         return $this->getParameter('registerToken');
     }
+
     public function setRegisterToken($value)
     {
         return $this->setParameter('registerToken', $value);
     }
+
     public function getShowOrderConfirmation()
     {
         return $this->getParameter('showOrderConfirmation');
     }
+
     public function setShowOrderConfirmation($value)
     {
         return $this->setParameter('showOrderConfirmation', $value);
     }
-    
+
     public function purchase(array $parameters = array())
     {
-        return $this->createRequest('\Omnipay\VerifoneOcius\Message\PurchaseRequest', $parameters);
+        return $this->createRequest(
+            '\Omnipay\VerifoneOcius\Message\PurchaseRequest',
+            $parameters
+        );
     }
+
     public function completePurchase(array $parameters = array())
     {
-        return $this->createRequest('\Omnipay\VerifoneOcius\Message\CompletePurchaseRequest', $parameters);
+        return $this->createRequest(
+            '\Omnipay\VerifoneOcius\Message\CompletePurchaseRequest',
+            $parameters
+        );
     }
+
     public function refund(array $parameters = array())
     {
-        return $this->createRequest('\Omnipay\VerifoneOcius\Message\RefundRequest', $parameters);
+        return $this->createRequest(
+            '\Omnipay\VerifoneOcius\Message\RefundRequest',
+            $parameters
+        );
     }
 }
