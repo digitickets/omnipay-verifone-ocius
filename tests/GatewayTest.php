f6fefb2e7779236a944f95308c6708dd79fa6bd4
Andy Coates <andycoatz@gmail.com>
2015-09-22 10:55:58 +0100

    Added required property

--- tests/GatewayTest.php
+++ tests/GatewayTest.php
@@ -9,6 +9,7 @@ class GatewayTest extends GatewayTestCase
     public function setUp()
     {
         parent::setUp();
+        $this->gateway = new Gateway($this->getHttpClient(), $this->getHttpRequest());
     }
 
     public function testStub()
